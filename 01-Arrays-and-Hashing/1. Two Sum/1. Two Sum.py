9cf2a1adc3d02def393ed75ac0d8986d8ec37bbf
Rohan Ram <34376023+RohanRam@users.noreply.github.com>
2026-07-21 21:27:36 +0530

    Update 1. Two Sum.py

--- 01-Arrays-and-Hashing/1. Two Sum/1. Two Sum.py
+++ 01-Arrays-and-Hashing/1. Two Sum/1. Two Sum.py
@@ -12,3 +12,17 @@ class Solution {
       return null;       
     }
 }
+
+
+
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         seen = {}
+#         for i , num in enumerate(nums):
+#             comp = target - num
+
+#             if comp in seen :
+#                 return [seen[comp],i]
+            
+#             seen[num] = i 
