f4aca5322c0fa5e6e7f7102ec46e7f22c1ec2a28
Rohan Ram <34376023+RohanRam@users.noreply.github.com>
2026-07-24 22:39:54 +0530

    [15] [Time Beats: 69.85%] [Memory Beats: 53.39%] - Leet2Hub

--- 01-Arrays-and-Hashing/15. 3Sum/15. 3Sum.py
+++ 01-Arrays-and-Hashing/15. 3Sum/15. 3Sum.py
@@ -1,15 +1,32 @@
 1class Solution:
 2    def threeSum(self, nums: list[int]) -> list[list[int]]:
 3        n=len(nums)
-4
-5        out=[]
-6        for i in range (0,n):
-7            for j in range(i+1,n):
-8                for k in range(j+1,n):
-9                    if ((nums[i] + nums[j] + nums[k]) == 0):
-10                        curr=sorted([nums[i] , nums[j] , nums[k]])
-11                        if curr not in out:
-12                            out.append(curr)
-13        return out
-14
-15        
+4        out=[]
+5        nums.sort()
+6        for i in range(n-2):
+7            if i>0 and nums[i] == nums[i-1]:
+8                continue
+9            
+10            left = i+1
+11            right = n-1
+12
+13            while left < right:
+14                total = nums[i] + nums[left] + nums[right]
+15                if total > 0:
+16                    right = right-1
+17                elif total < 0:
+18                    left = left+1
+19                else :
+20                    out.append([nums[i],nums[left],nums[right]])
+21
+22                    while left < right and nums[left] == nums[left+1]:
+23                        left =left+1
+24                    while right > left and nums[right] == nums[right-1]:
+25                        right-=1
+26                    left += 1
+27                    right -= 1
+28        
+29        return out
+30
+31
+32        
