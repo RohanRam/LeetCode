52c62f6d87575a8e6c7bece1c6e30f5028a96724
Rohan Ram <34376023+RohanRam@users.noreply.github.com>
2026-09-22 19:00:56 +0530

    [9] [Time Beats: 64%] [Memory Beats: 29.10%] - Leet2Hub

--- 13-Math/9. Palindrome Number/9. Palindrome Number.py
+++ 13-Math/9. Palindrome Number/9. Palindrome Number.py
@@ -1,14 +1,25 @@
-class Solution:
-    def isPalindrome(self, n: int) -> bool:
-        if n<0:
-            return False
-        original = n  
-        rev = 0
-        while n >0:
-            digit = n% 10
-            rev = rev * 10 + digit
-            n //= 10 
-        return original==rev
-
-
-        
+1# class Solution:
+2#     def isPalindrome(self, x: int) -> bool:
+3#         s = str(x)
+4#         r=s[::-1]
+5#         if s == r:
+6#             return True
+7#         else :
+8#             return False
+9        
+10
+11class Solution:
+12    def isPalindrome(self, x: int) -> bool:
+13        if x < 0 or (x % 10 == 0 and x != 0):
+14            return False
+15        og=x
+16        r=0
+17        while x > 0 :
+18            digit = x % 10 
+19            r = r * 10 + digit
+20            x = x // 10  
+21
+22        if r == og :
+23            return True
+24        else:
+25            return False  
