5f5b22f99fcfe64d3f52024a20cb1d10e4baf835
Rohan Ram <34376023+RohanRam@users.noreply.github.com>
2026-09-23 23:29:22 +0530

    [627] [Time Beats: 95.53%] - Leet2Hub

--- 627. Swap Sex of Employees/627. Swap Sex of Employees.sql
+++ 627. Swap Sex of Employees/627. Swap Sex of Employees.sql
@@ -1,1 +1,10 @@
-update Salary set sex = case    when sex = 'm' then 'f'    when sex = 'f' then 'm'end;
+1-- update Salary 
+2-- set sex = case
+3--     when sex = 'm' then 'f'
+4--     when sex = 'f' then 'm'
+5-- end;
+6
+7update Salary 
+8set sex = IF(sex = 'f','m','f');
+9
+10
